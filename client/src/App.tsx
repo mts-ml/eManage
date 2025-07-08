6e4a9a4e1864495febdad9e193f1db108383de1b
Mateus <mtsmlima@gmail.com>
2025-07-08 15:57:50 -0300

    Add new routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,12 @@
-import { Route, Routes } from "react-router"
+import { Route, Routes } from "react-router-dom"
 
 import ErrorPage from "./pages/ErrorPage"
 import { Layout } from "./pages/Layout"
 import { Login } from "./pages/Login"
 import { Register } from "./pages/Register"
 import { MainPageLayout } from "./pages/MainPageLayout"
+import { RouteAuthentication } from "./components/RouteAuthentication"
+import { Unauthorized } from "./pages/Unauthorized"
 
 
 function App() {
@@ -13,8 +15,12 @@ function App() {
       <Route element={<Layout />} errorElement={<ErrorPage />}>
         <Route index element={<Login />} />
         <Route path="register" element={<Register />} />
-        <Route path="main" element={<MainPageLayout />} />
 
+        <Route element={<RouteAuthentication />}>
+          <Route path="main" element={<MainPageLayout />} />
+        </Route>
+
+        <Route path='unauthorized' element={<Unauthorized />} />
         <Route path="*" element={<ErrorPage />} />
       </Route>
     </Routes>
