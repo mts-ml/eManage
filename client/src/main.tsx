6840b5881b172b957077e0dd7ea657817170e74f
Mateus <mtsmlima@gmail.com>
2025-07-08 15:57:43 -0300

    Add provider

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,15 +1,18 @@
-// import { StrictMode } from 'react'
+import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from "react-router";
+import { BrowserRouter } from "react-router-dom"
+import { AuthProvider } from './Context/AuthContext'
 import App from './App'
 
 import './index.css'
 
 
 createRoot(document.getElementById('root')!).render(
-  <BrowserRouter>
-   {/* <StrictMode> */}
-      <App />
-  {/* </StrictMode>, */}
-  </BrowserRouter>
+  <StrictMode>
+    <AuthProvider>
+      < BrowserRouter >
+        <App />
+      </BrowserRouter >
+    </AuthProvider>
+  </StrictMode>
 )
