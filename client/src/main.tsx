5e2178796bf0110bfa815680f63b9ad22bb76a89
Mateus <mtsmlima@gmail.com>
2025-07-30 16:49:20 -0300

    Add new provider to main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,11 +4,12 @@ import { BrowserRouter } from "react-router-dom"
 
 import App from './App'
 import { AuthProvider } from './Context/AuthContext'
-import { ClientsProvider } from './Context/ClientsContext'
+import { ClientsProvider } from './Context/ClientContext'
 import { ProductsProvider } from './Context/ProductsContext'
 import { SaleProvider } from './Context/SaleContext'
 
 import './index.css'
+import { SupplierProvider } from './Context/SupplierContext'
 
 
 createRoot(document.getElementById('root')!).render(
@@ -16,11 +17,13 @@ createRoot(document.getElementById('root')!).render(
   < BrowserRouter >
     <AuthProvider>
       <ClientsProvider>
-        <ProductsProvider>
-          <SaleProvider>
-            <App />
-          </SaleProvider>
-        </ProductsProvider>
+        <SupplierProvider>
+          <ProductsProvider>
+            <SaleProvider>
+              <App />
+            </SaleProvider>
+          </ProductsProvider>
+        </SupplierProvider>
       </ClientsProvider>
     </AuthProvider >
   </BrowserRouter >
