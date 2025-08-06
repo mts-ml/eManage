29d20cf0cb8fd35fff6d25bb1221003629cede0b
Mateus <mtsmlima@gmail.com>
2025-08-06 15:28:44 -0300

    Add new route for DELETE method in purchase.ts

--- server/src/routes/api/purchases/purchases.ts
+++ server/src/routes/api/purchases/purchases.ts
@@ -1,7 +1,7 @@
 import express from 'express'
 
 import { handleTransactionValidation } from '../../../middleware/transactionValidation.js'
-import { createNewPurchase, deletePurchase, getAllPurchases, updatePurchase } from '../../../controller/purchaseController.js'
+import { createNewPurchase, deletePurchase, getAllPurchases, getPurchasesHistory, updatePurchase } from '../../../controller/purchaseController.js'
 
 
 const router = express.Router()
@@ -9,7 +9,8 @@ const router = express.Router()
 router.route('/')
     .get(getAllPurchases)
     .post(handleTransactionValidation, createNewPurchase)
+router.get('/history', getPurchasesHistory)
 router.put('/:id', handleTransactionValidation, updatePurchase)
-router.delete('/:id', handleTransactionValidation, deletePurchase)
+router.delete('/:id', deletePurchase)
 
 export default router
