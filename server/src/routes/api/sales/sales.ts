3d25d49fe1330d459e3490967fb2b00b0f7bc4d1
Mateus <mtsmlima@gmail.com>
2025-08-07 14:40:20 -0300

    Fix middleware improts name in purchases.ts and sales.ts

--- server/src/routes/api/sales/sales.ts
+++ server/src/routes/api/sales/sales.ts
@@ -1,6 +1,6 @@
 import express from 'express'
 
-import { handleTransactionValidation } from '../../../middleware/transactionValidation.js'
+import { handleSaleValidation } from '../../../middleware/transactionValidation.js'
 import { createNewSale, deleteSale, getAllSales, getSalesHistory, updateSale } from '../../../controller/saleController.js'
 
 const router = express.Router()
@@ -8,9 +8,9 @@ const router = express.Router()
 
 router.route('/')
     .get(getAllSales)
-    .post(handleTransactionValidation, createNewSale)
+    .post(handleSaleValidation, createNewSale)
 router.get('/history', getSalesHistory)
-router.put('/:id', handleTransactionValidation, updateSale)
+router.put('/:id', handleSaleValidation, updateSale)
 router.delete('/:id', deleteSale)
 
 export default router
