afabb1d632ecd866ecf1af9bd58b8fc25c242428
Mateus <mtsmlima@gmail.com>
2025-08-12 13:10:11 -0300

    Add lazy loading to AuthProvider in Router.tsx

--- client/src/Router.tsx
+++ client/src/Router.tsx
@@ -6,13 +6,13 @@ import { Login } from "./pages/Login"
 import { Register } from "./pages/Register"
 import { Home } from "./pages/Home"
 import { Unauthorized } from "./pages/Unauthorized"
-import { AuthProvider } from "./Context/AuthContext"
 import { Loading } from "./components/ReactLoader"
 import ErrorPage from "./pages/ErrorPage"
 import { RouteAuthentication } from "./components/RouteAuthentication"
 import { ROLES_LIST } from "./config/roles_list"
 
 // Lazy load dos contexts pesados
+const AuthProvider = lazy(() => import('./Context/AuthContext').then((module) => ({ default: module.AuthProvider })))
 const ClientsProvider = lazy(() => import('./Context/ClientContext').then(module => ({ default: module.ClientsProvider })))
 const SupplierProvider = lazy(() => import('./Context/SupplierContext').then(module => ({ default: module.SupplierProvider })))
 const ProductsProvider = lazy(() => import('./Context/ProductsContext').then(module => ({ default: module.ProductsProvider })))
@@ -22,29 +22,31 @@ const ExpensesProvider = lazy(() => import('./Context/ExpensesContext').then(mod
 
 function withProviders(children: React.ReactNode) {
    return (
-      <AuthProvider>
-         <Suspense fallback={<Loading />}>
-            <ClientsProvider>
-               <Suspense fallback={<Loading />}>
-                  <SupplierProvider>
-                     <Suspense fallback={<Loading />}>
-                        <ProductsProvider>
-                           <Suspense fallback={<Loading />}>
-                              <SaleProvider>
-                                 <Suspense fallback={<Loading />}>
-                                    <ExpensesProvider>
-                                       {children}
-                                    </ExpensesProvider>
-                                 </Suspense>
-                              </SaleProvider>
-                           </Suspense>
-                        </ProductsProvider>
-                     </Suspense>
-                  </SupplierProvider>
-               </Suspense>
-            </ClientsProvider>
-         </Suspense>
-      </AuthProvider>
+      <Suspense fallback={<Loading />}>
+         <AuthProvider>
+            <Suspense fallback={<Loading />}>
+               <ClientsProvider>
+                  <Suspense fallback={<Loading />}>
+                     <SupplierProvider>
+                        <Suspense fallback={<Loading />}>
+                           <ProductsProvider>
+                              <Suspense fallback={<Loading />}>
+                                 <SaleProvider>
+                                    <Suspense fallback={<Loading />}>
+                                       <ExpensesProvider>
+                                          {children}
+                                       </ExpensesProvider>
+                                    </Suspense>
+                                 </SaleProvider>
+                              </Suspense>
+                           </ProductsProvider>
+                        </Suspense>
+                     </SupplierProvider>
+                  </Suspense>
+               </ClientsProvider>
+            </Suspense>
+         </AuthProvider>
+      </Suspense>
    )
 }
 
